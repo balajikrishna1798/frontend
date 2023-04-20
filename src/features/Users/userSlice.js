30f7433dae38af96af07d2721376667a309e5972
balajikrishna1798 <balajikrishna44589@gmail.com>
2023-04-20 18:17:10 +0530

    frontend

---
+++ src/features/Users/userSlice.js
@@ -0,0 +1,10 @@
+import {createAsyncThunk,createSlice} from '@reduxjs/toolkit'
+
+export const authSlice = createSlice({
+    name:"auth",
+    initialState:"",
+    reducers:{},
+    extraReducers:(builder)=>{
+        
+    }
+})
